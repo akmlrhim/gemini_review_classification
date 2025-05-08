b2b7e221c4a09eacec56a9c793c6a7b340714550
rahimmsz <150146538+rahimmsz@users.noreply.github.com>
2025-05-08 12:41:48 +0800

    overflow for tfidf

--- resources/views/preprocessing/tf-idf.blade.php
+++ resources/views/preprocessing/tf-idf.blade.php
@@ -1,5 +1,8 @@
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot:title>
-
-  <pre>{{ json_encode($tf_idf, JSON_PRETTY_PRINT) }}</pre>
+	<div class="overflow-x-auto">
+		<pre class="p-4 bg-gray-100 rounded-md">
+			{{ json_encode($tf_idf, JSON_PRETTY_PRINT) }}
+		</pre>
+	</div>
 </x-layout>
