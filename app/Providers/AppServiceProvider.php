5c46d0b2f5c9d7bf9c0eb30aa8518026aec21305
rahimmsz <150146538+rahimmsz@users.noreply.github.com>
2025-07-05 18:09:58 +0800

    php ini config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,10 @@ class AppServiceProvider extends ServiceProvider
 		if (env('APP_ENV') === 'production') {
 			URL::forceScheme('https');
 		}
+
+		ini_set('memory_limit', '512M');
+		ini_set('max_execution_time', '300');
+		ini_set('upload_max_filesize', '100M');
+		ini_set('post_max_size', '100M');
 	}
 }
