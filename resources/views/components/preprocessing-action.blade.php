3e97fa012d98f71b8e0fb8e86ac758358d880a4d
rahimmsz <150146538+rahimmsz@users.noreply.github.com>
2025-07-06 07:31:59 +0800

    Building Assets

--- resources/views/components/preprocessing-action.blade.php
+++ resources/views/components/preprocessing-action.blade.php
@@ -111,7 +111,7 @@
               value="{{ old('file') }}"
               class="w-full text-xs text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" />
             @error('file')
-              <small class="text-red-500 text-xs"> {{ $message }}</small>
+              <small class="text-red-500 font-medium text-xs"> {{ $message }}</small>
             @enderror
           </div>
 
